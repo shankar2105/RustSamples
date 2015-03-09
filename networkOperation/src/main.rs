a3811174bb608523d8a93c68706d29020b3b8b39
Shankar <shankar21mail@gmail.com>
2015-03-09 12:28:47 +0530

    Port changed in println!.

--- networkOperation/src/main.rs
+++ networkOperation/src/main.rs
@@ -1,7 +1,7 @@
 use std::old_io::TcpStream;
 
 fn main() {
-    println!("TCP Client Connecting to 127.0.0.1:8000...");
+    println!("TCP Client Connecting to 127.0.0.1:8080...");
     let mut socket = TcpStream::connect("127.0.0.1:8080").unwrap();
     socket.write_all(b"GET /maidsafe.png HTTP/1.0\n\n");
     let response = socket.read_to_end();
